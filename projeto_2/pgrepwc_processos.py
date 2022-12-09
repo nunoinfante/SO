4c47708096280a4fc06423fddf66010d5b53aa1d
fc55411 <fc55411@alunos.fc.ul.pt>
2022-12-09 08:16:47 +0000

    proj2

--- projeto_2/pgrepwc_processos.py
+++ projeto_2/pgrepwc_processos.py
@@ -2,14 +2,34 @@
 ### Aluno 1: Nuno Infante (fc55411)
 
 from multiprocessing import Process, Array, Queue, Lock, Value
+from re import findall, compile
 import argparse
 import unicodedata
 import os
-from signal import signal, SIGINT
+from signal import signal, SIGINT, setitimer, SIGALRM, ITIMER_REAL
 from time import time
+from functools import partial
 
-stop = False
+inicio = time()
+end = Value('i', 0)
 STOP_TOKEN = 'STOP'
+
+#Criamos dois arrays que vão estar na memória partilhada para cada ficheiro a ser pesquisado,
+    #um para as palavras encontradas e outro para as linahs encontradas 
+palavras_encontradas = Value('i', 0)
+linhas_encontradas = Value('i', 0)
+
+files_done = Value('i', 0)
+
+def seconds_to_micro(sec):
+    """
+    Obtém segundos e retorna em microsegundos
+    Args:
+        sec: número em segundos
+    Returns:
+        Número em microsegundos
+    """
+    return int(sec*1000000)
 
 def obter_argumentos():
     """
@@ -51,7 +71,14 @@ def obter_argumentos():
     return args
 
 def remover_acentos(texto):
-    return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode("utf-8").lower()
+    """
+    Remove acentos a uma string
+    Args:
+        texto: string com o texto a ser pesquisado
+    Returns:
+        string do texto sem acentos
+    """
+    return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').decode("utf-8")
 
 def encontrar_palavras(palavra, texto):
     """
@@ -60,32 +87,35 @@ def encontrar_palavras(palavra, texto):
         palavra: string, onde é a palavra que vamos pesquisar
         ficheiro: string, ficheiro onde vamos pesquisar
     Returns:
-        Um tuplo com as linhas onde foram encontradas palavras, o número de ocorrências da palavra
-        e o número de linhas encontradas
+        Um tuplo com o número de ocorrências isoladas e o número de linhas em que 'palavra' ocorreu
     """
 
-    linhas_com_palavra = []
-    numero_palavras = 0
+    linhas = []
+    palavras = []
 
-    for s in texto:
-        s = remover_acentos(s)
-        if remover_acentos(palavra) in s:
-            linhas_com_palavra.append(s)
-            numero_palavras += s.count(remover_acentos(palavra))
+    #Compila a palavra numa expressão regular
+    regex = compile(f'\\b{remover_acentos(palavra)}\\b')
 
-    return (numero_palavras, len(linhas_com_palavra))
+    for linha in texto:
+        linha = remover_acentos(linha)
+        #Pesquisa em linha usando a expressão regular
+        palavras += regex.findall(linha)
+        #Se a palavra estiver em linha damos append
+        if palavra in linha:
+            linhas.append(linha)
 
-def grepwc(args, palavras_encontradas, linhas_encontradas, ficheiros=[], texto=''):
+    return (len(palavras), len(linhas))
+
+def grepwc(args, ficheiros=[], texto=''):
     """
-    Imprime as linhas encontradas de cada ficheiro, bem como o número de ocorrências da palavra e o número de linhas encontradas
+    Faz a pesquisa, caso -e seja utilizado, com um texto recebido pela queue. Caso contrário itera sobre os ficheiros dados e faz a pesquisa
     Args:
         args: objeto da classe ArgumentParser, com os argumentos dados pelo utilizador
-        ficheiros: lista de strings, com o nome dos ficheiros onde vamos procurar
-        palavras_encontradas: ista com o número de ocorrências para cada ficheiro
-                              Este argumento está alocado na memória partilhada
-        linhas_encontradas: lista com o número de linhas encontradas para cada ficheiro
-                            Este argumento está alocado na memória partilhada
+        ficheiros: lista de strings, com o nome dos ficheiros onde vamos procurar. Se -e não seja utilizado
+        texto: lista de strings, com o contéudo do(s) ficheiro(s) a pesquisar. Se -e for utilizado
     """
+
+    #Se -e for utilizado
     if args.bytes:
         numero_palavras, numero_linhas = encontrar_palavras(args.palavra, texto)
 
@@ -94,18 +124,30 @@ def grepwc(args, palavras_encontradas, linhas_encontradas, ficheiros=[], texto='
 
     else:
         for f in ficheiros:
-            print(f)
-            texto = open(f, 'r').readlines()
+            texto = read_file(f)
 
             numero_palavras, numero_linhas = encontrar_palavras(args.palavra, texto)
             palavras_encontradas.value += numero_palavras
             linhas_encontradas.value += numero_linhas
 
-            if stop:
+            #Após o ficheiro ser processado completamente, somamos à variável em memória partilhada para ser usada na impressão de resultados
+            files_done.value += 1
+
+            #Quando o SIGINT (CTRL-C) é pressionado
+            if end.value == 1:
                 break
              
-###########################################################
-def get_size_process(tarefas, dict):
+
+def min_bytes_processo(tarefas, dict):
+    """
+    Obter o index do processo com menor tamanho total de ficheiros
+
+    Args:
+        tarefas: lista com listas com o nome de ficheiros associado a cada processo
+        dict: dicionário com pares ficheiro:tamanho de todos os ficheiros dados
+    Returns:
+        Index do processo
+    """
     res = []
     for p in tarefas:
         counter = 0
@@ -119,17 +161,14 @@ def get_size_process(tarefas, dict):
 
 def dividir_tarefas(args):
     """
-    Distribui os ficheiros a serem procurados pelo número de processos.
-    Os ficheiros são distribuídos uniformemente por cada processo. Por exemplo, dados 3 ficheiros e 2 processos, 
-    os ficheiros 1 e 2 vão para o primeiro processo e o ficheiro 3 vai para o segundo processo    
+    Distribui os ficheiros a serem procurados pelo número de processos, tendo em conta o seu tamanho.
     
     Args:
         args: objeto da classe ArgumentParser, com os argumentos dados pelo utilizador
     Returns: 
-        lista com várias listas contendo strings dos nomes dos ficheiros a serem procurados, para cada processo
+        lista com listas com o nome de ficheiros associado a cada processo
     """
 
-    ###############################################
     num_ficheiros = len(args.ficheiros)
     num_processos = args.processos
     tarefas =  [[]*num_processos for i in range(num_processos)]
@@ -139,111 +178,152 @@ def dividir_tarefas(args):
         args.processos = num_ficheiros
         tarefas = [[ficheiro] for ficheiro in args.ficheiros]
 
-    ###########################################################
+    #Se o número de processos for menor que o número de ficheiros
     elif num_processos < num_ficheiros:
         dict = {}
         for f in args.ficheiros:
-            dict[f] = os.stat(f).st_size
-        dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])}
-        dict2 = dict.copy()
+            dict[f] = os.stat(f).st_size #Associar cada ficheiro ao seu tamanho em bytes
+        dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])} #Ordenar o dicionário do menor para o maior
+        dict2 = dict.copy() #Copia do dicionário
         
         while len(dict) != 0:
-            index_min = get_size_process(tarefas, dict2)
-            tarefas[index_min].append(list(dict.keys())[-1])
-            dict.popitem()
+            index_min = min_bytes_processo(tarefas, dict2) #Index do processo com menor tamanho total de ficheiros
+            tarefas[index_min].append(list(dict.keys())[-1]) #Acrescentar ao index obtido o nome do ficheiro com menor tamanho
+            dict.popitem() # Retirar o par key:value com menor tamanho
 
     return tarefas
 
-def readFile(file):
+
+def read_file(file):
+    """
+    Lê um ficheiro
+
+    Args:
+        file: ficheiro a ser lido
+    Returns:
+        lista com strings de cada linha lida
+    """
     with open(file, 'r') as input:
         return input.readlines()
 
-def numberLinesFiles(files):
+
+def calcula_numero_linhas(files):
+    """
+    Cálcula o número de linhas para todos os ficheiros dados
+
+    Args:
+        files: lista com ficheiros dados pelo utilizador 
+    Returns:
+        Número de linhas dos ficheiros
+    """
     counter = 0
     for f in files:
-        counter += len(readFile(f))
+        counter += len(read_file(f))
     return counter
 
-def numberBytesStringList(l):
+
+def numero_bytes_string(lista):
+    """
+    Cálcula o número de bytes totais de uma lista de strings
+
+    Args:
+        lista: lista de strings
+    Returns:
+        Número de bytes 
+    """
     counter = 0
-    for s in l:
-        counter += len(s.encode('utf-8'))
+    for string in lista:
+        counter += len(string.encode('utf-8'))
     return counter
+
 
 def produtor(files, queue, maxBytes, queueSize):
-    break_out_flag = False
     i = 0
     list = []
+
     for f in files:
-        texto = readFile(f)
+        print(f)
+        texto = read_file(f)
         for s in texto:
             list.append(s)
             i += 1
-            if numberBytesStringList(list) > maxBytes:
+            if numero_bytes_string(list) > maxBytes:
                 while queueSize.value > 1000000:
                     pass
-                queueSize.value += numberBytesStringList(list[:-1])
+                queueSize.value += numero_bytes_string(list[:-1])
                 queue.put(list[:-1])
                 list = list[-1:]
-            if stop:
-                break_out_flag = True
-                break
-        if break_out_flag:
-            break
-        if i == numberLinesFiles(files):
+        if i == calcula_numero_linhas(files):
                 queue.put(list)
-                queueSize.value += numberBytesStringList(list)
+                queueSize.value += numero_bytes_string(list)
+
+        files_done.value += 1
+        if end.value == 1:
+            queue.put(list)
+            break
+
     queue.put(STOP_TOKEN)
     
 
-def consumidor(queue, lock, args, palavras_encontradas, linhas_encontradas, queueSize):
+def consumidor(queue, lock, args, queueSize):
     while True:
         item = queue.get()
         lock.acquire()
-        queueSize.value -= numberBytesStringList(item)
+        queueSize.value -= numero_bytes_string(item)
         lock.release()
         if item == STOP_TOKEN:
             queue.put(STOP_TOKEN) #Põe se de volta na queue para informar os outros consumidores
             break
         else:
             lock.acquire()
-            grepwc(args, palavras_encontradas, linhas_encontradas, texto=item)
+            grepwc(args, texto=item)
             lock.release()
-    
 
-def interval():
-    while not stop:
-        print('A')
+
+def interval(args, sig, null):
+    if args.c:
+        print(f'Número de ocorrências da palavra {args.palavra}: {palavras_encontradas.value}')
+    if args.l:
+        print(f'Número de linhas resultante da palavra {args.palavra}: {linhas_encontradas.value}')
+
+    print(f'Número de ficheiros completamente processados: {files_done.value}')
+    print(f'Número de ficheiros ainda em processamento: {len(args.ficheiros) - files_done.value}')
+    print(f'Tempo decorrido desdo o início da execução: {seconds_to_micro(time() - inicio)}\n')
+
 
 def sigint(sig, null):
-    global stop
-    stop = True
+    """
+    Altera o valor de end, para parar o processamento do resto dos ficheiros
 
-def pgrepwc(args, palavras_encontradas, linhas_encontradas):
+    Args:
+        sig: obrigatório pelo sinal
+        null: obrigatório pelo sinal
+    """
+    end.value = 1
+
+
+def pgrepwc(args):
     """
     Executa a função grepwc de forma paralela criando o número de processos filho indicado nos argumentos dados pelo utilizador
     Args:
         args: objeto da classe ArgumentParser, com os argumentos dados pelo utilizador
-        palavras_encontradas: caso a opção -e esteja específicada então palavras_encontradas vai ser um int,
-                              caso contrário vai ser uma lista com o número de ocorrências para cada ficheiro
-                              Este argumento está alocado na memória partilhada
-        linhas_encontradas: caso a opção -e esteja específicada então linhas_encontradas vai ser um int,
-                            caso contrário vai ser uma lista com o número de linhas encontradas para cada ficheiro
-                            Este argumento está alocado na memória partilhada
+        palavras_encontradas: Value com o número de ocorrências isoladas, que se encontra em memória partilhada
+        linhas_encontradas: Value com o número de linhas em que houve ocorrências, que se encontra em memória partilhada
+        files_done: Value com o número de ficheiros já processados completamente
     """
 
     processos_filho = []
 
+    #Se a opção -e for utilizada
     if args.bytes:
         queue = Queue()
-
-        queueSize = Value('i', 0)
+        queueSize = Value('i', 0) #Valor para verificar que o tamanho da queue não contenha mais que 1MB de dados
 
         processos_filho = []
         lock = Lock()
 
         for i in range(args.processos):
-            processos_filho.append(Process(target=consumidor, args = (queue, lock, args, palavras_encontradas, linhas_encontradas, queueSize)))
+            processos_filho.append(Process(target=consumidor, args = (queue, lock, args, queueSize)))
         
         prod = Process(target=produtor, args = (args.ficheiros, queue, args.bytes, queueSize))
 
@@ -259,45 +339,38 @@ def pgrepwc(args, palavras_encontradas, linhas_encontradas):
         tarefas = dividir_tarefas(args)
         for i in range(args.processos):
             processos_filho.append(Process(target=grepwc,
-                                        args = (args, palavras_encontradas, linhas_encontradas, tarefas[i])))
+                                        args = (args, tarefas[i])))
 
         for i in range(args.processos):
             processos_filho[i].start()
         for i in range(args.processos):
             processos_filho[i].join()
 
+
 def main():
-    inicio = time()
-    print('Programa: pgrepwc_processos.py')
+    print('Programa: pgrepwc_processos.py\n')
 
     args = obter_argumentos()
 
     signal(SIGINT, sigint)
 
-    #Criamos dois arrays que vão estar na memória partilhada para cada ficheiro a ser pesquisado,
-    #um para as palavras encontradas e outro para as linahs encontradas 
-    palavras_encontradas = Value('i', 0)
-    linhas_encontradas = Value('i', 0)
-
-    interval_process = Process(target=interval)
-    interval_process.start()
+    signal(SIGALRM, partial(interval, args))
+    setitimer(ITIMER_REAL, 3, 3)
 
     #Se o número de processos for igual a 1 então o processo pai faz a pesquisa
     if args.processos == 1:
-        grepwc(args, palavras_encontradas, linhas_encontradas, ficheiros = args.ficheiros)
+        grepwc(args, ficheiros = args.ficheiros)
     else:   
-        pgrepwc(args, palavras_encontradas, linhas_encontradas)
-
-    print("\nNo total, em todos os ficheiros foram encontradas:")
+        pgrepwc(args)
 
     if args.c:
-        print(f"{palavras_encontradas.value} ocorrencias da palavra {args.palavra}")
-    if args.l: 
-        print(f"{linhas_encontradas.value} linhas com a palavra {args.palavra}")  
+        print(f'Número de ocorrências da palavra {args.palavra}: {palavras_encontradas.value}')
+    if args.l:
+        print(f'Número de linhas resultante da palavra {args.palavra}: {linhas_encontradas.value}')
 
-    print(f'Tempo de execucao: {time()-inicio}')
-
-
+    print(f'Número de ficheiros completamente processados: {files_done.value}')
+    print(f'Número de ficheiros ainda em processamento: {len(args.ficheiros) - files_done.value}')
+    print(f'Tempo decorrido desdo o início da execução: {seconds_to_micro(time() - inicio)}')
 
 if __name__ == "__main__":
     main()
